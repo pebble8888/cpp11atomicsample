9f1c4e17fed95297c5417ccfa70873ed03ed8201
pebble8888 <pebble8888@gmail.com>
2016-08-03 20:21:41 +0900

    modify

--- cpp11atomicsample/main.cpp
+++ cpp11atomicsample/main.cpp
@@ -9,41 +9,104 @@
 #include <iostream>
 #include <atomic>
 #include <thread>
-#include <chrono>
+#include <mutex>
+#include "StopWatch.h"
+#include <libkern/OSAtomic.h>
 
-uint8_t nonatomic_value;
+#include <Accelerate/Accelerate.h>
+
+std::atomic_short value = ATOMIC_VAR_INIT(0);
+struct Hoge {
+    int64_t a;
+    int64_t b;
+};
+
+Hoge hoge1 = { 25, 26 };
+Hoge hoge2;
+
+//int16_t value = 0;
+//std::mutex mtx;
+//OSSpinLock lock = OS_SPINLOCK_INIT;
 
 void process1() {
     bool flag = false;
-    for (int i = 0; i < 1000000; ++i){
-        std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    for (int i = 0; i < 100000; ++i){
+        std::this_thread::yield;
         if (flag){
-            nonatomic_value = 0xff; 
+            value.store(255, std::memory_order_release);
+            //std::lock_guard<std::mutex> lk(mtx);
+            //OSSpinLockLock(&lock);
+            //value = 32767;
+            //OSSpinLockUnlock(&lock);
         } else {
-            nonatomic_value = 0x0;
+            value.store(0, std::memory_order_release);
+            //std::lock_guard<std::mutex> lk(mtx);
+            //OSSpinLockLock(&lock);
+            //value = 0;
+            //OSSpinLockUnlock(&lock);
         }
         flag = !flag;
     }
 }
 
+void func1(Hoge hoge){
+    hoge2 = hoge;
+}
+
+void func2(const Hoge& hoge){
+    hoge2 = hoge;
+}
+
 void process2() {
-    for (int i = 0; i < 1000000; ++i){
-        std::this_thread::sleep_for(std::chrono::nanoseconds(1));
-        if (nonatomic_value != 0xff &&
-            nonatomic_value != 0x0){
-            printf("warningA! nonatomic_value %x\n", nonatomic_value);
+    for (int i = 0; i < 100000; ++i){
+        std::this_thread::yield;
+        func1(hoge1);
+        //func2(hoge1);
+        //OSSpinLockLock(&lock);
+        {
+            //std::lock_guard<std::mutex> lk(mtx);
+            //int16_t v = value;
+            //int16_t v = value.load(std::memory_order_acquire);
+            
+            //vDSP_vadd(a, 1, b, 1, a, 1, 512);
+            //OSSpinLockUnlock(&lock);
+            //if (v != 32767 && v != 0){
+            /*
+            if (value != 32767 && value != 0){
+                printf("warningA! i %d v %d\n", i, value);
+            }
+             */
         }
     }
 }
 
 int main(int argc, char** argv) {
-    printf("start address %p\n", &nonatomic_value);
-    nonatomic_value = 0;
-    std::thread t1(process1);
-    std::thread t2(process2);
-    t1.join();
-    t2.join();
+    long min = 10000000;
+    long max = 0;
+    long sum = 0;
+    long count = 20;
+    for (int i = 0; i < count; ++i){
+        //value.store(0);
+        StopWatch sw;
+        sw.start();
+        //std::thread t1(process1);
+        std::thread t2(process2);
+        //t1.join();
+        t2.join();
+        sw.stop();
+        //printf("%s\n", sw.description().c_str());
+        long us = sw.microseconds();
+        //printf("%ld\n", us);
+        
+        if (us < min){
+            min = us;
+        }
+        if (max < us){
+            max = us;
+        }
+        sum += us;
+    }
+    printf("min %d us max %d us mean %d us\n", min, max, sum/count);
     
-    printf("end\n");
     return 0;
 }
